e48f6c4ea6fa8c52b1b04b300be5115e7db32829
zsofi <zsofiath@gmail.com>
2021-12-11 17:50:22 +0100

    :recycle: not using array

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -3,21 +3,15 @@ export function absoluteValuesSumMinimization(a: number[]): number {
   let sumPrev = 8001;
   let i = 0;
 
-  let results = [];
-
   while (sumPrev > sum) {
     sumPrev = sum;
     sum = 0;
     for (const number of a) {
       sum += Math.abs(number - i);
     }
-
-    results.push({ num: i, sum: sum });
-
     i++;
   }
-  results.sort((prev, curr) => prev.sum - curr.sum);
-  return results[0].num;
+  return i-2;
 }
 
 console.log(absoluteValuesSumMinimization([2, 4, 7]));

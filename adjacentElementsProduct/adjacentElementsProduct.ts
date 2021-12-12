704ab2593f6d946b7985ccdf4db727db8b47c598
zsofi <zsofiath@gmail.com>
2021-12-12 13:43:15 +0100

    :tada: adjacent elements product

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,5 +1,11 @@
 export function adjacentElementsProduct(inputArray: number[]): number {
-
+let max = -Infinity;
+inputArray.sort((p, n) => {
+    if(p*n > max) max = p*n;
+    return 1;
+})
+return max;
 }
 
-//console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
+console.log(adjacentElementsProduct([-1,10,-1,2]));

94d4b88e01a9203343adab943f1009a22cff5b82
zsofi <zsofiath@gmail.com>
2021-12-12 13:54:54 +0100

    :tada: all longest strings

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,5 +1,18 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+    let strings = [inputArray[0]];
+    let longest = strings[0];
+    for (const str of inputArray.splice(1)) {
+        if(str.length === longest.length) { 
+            strings.push(str);
+        }
+        else if(str.length > longest.length) { 
+            strings = [str];
+            longest = str;
+        }
+    }
 
+    return strings;
 }
 
 console.log(allLongestStrings(["aba", "aa", "ad", "vcd", "aba"]));
+console.log(allLongestStrings(["aba", "aa", "adaa", "vcd", "aba"]));

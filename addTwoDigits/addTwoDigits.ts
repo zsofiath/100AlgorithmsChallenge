f6b9fb706fe18d175da299b7e834d8847ed3a893
zsofi <zsofiath@gmail.com>
2021-12-12 12:10:51 +0100

    :tada: add two digits

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,7 @@
-export function addTwoDigits(n: any): number {
-
+export function addTwoDigits(n: any): number {  
+    return n.toString().split('').reduce((a, c) => {        
+        return +a + +c; // !!! String always wins when auto converting
+    }, 0)
 }
 
-// console.log(addTwoDigits(29));
+ console.log(addTwoDigits(29));

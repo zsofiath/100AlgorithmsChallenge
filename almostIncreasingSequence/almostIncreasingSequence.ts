4c0da075e7c5ac12f3bcd5ca43614c3afc1630b4
zsofi <zsofiath@gmail.com>
2021-12-12 16:04:13 +0100

    :tada: almost increasing sequence

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,6 +1,12 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+    let mistakeNum = 0;
+  sequence.reduce((p, c) => {
+    mistakeNum += p > c ? 1 : 0;
 
+    return c;
+  });
+  return mistakeNum < 2;
 }
 
-// console.log(almostIncreasingSequence([1, 3, 2, 1])) 
-// console.log(almostIncreasingSequence([1, 3, 2])) 
+console.log(almostIncreasingSequence([1, 3, 2, 1]));
+console.log(almostIncreasingSequence([1, 3, 2]))

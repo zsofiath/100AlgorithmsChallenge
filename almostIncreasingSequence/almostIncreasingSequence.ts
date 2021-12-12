ae4b22bbc83036d269ea2ec295c0658fae2f8ce4
zsofi <zsofiath@gmail.com>
2021-12-12 19:58:20 +0100

    :bulb: optimisation

--- almostIncreasingSequence/almostIncreasingSequence.ts
+++ almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,12 +1,17 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
-    let mistakeNum = 0;
-  sequence.reduce((p, c) => {
+  let mistakeNum = 0;
+  try {
+    sequence.reduce((p, c) => {
     mistakeNum += p > c ? 1 : 0;
-
+    if (mistakeNum == 2) throw Error("2 mistakes");
     return c;
   });
+  } catch (error) {
+    return false;
+  }
+  
   return mistakeNum < 2;
 }
 
 console.log(almostIncreasingSequence([1, 3, 2, 1]));
-console.log(almostIncreasingSequence([1, 3, 2]))
+console.log(almostIncreasingSequence([1, 3, 2]));

0edbe691703bec2230c6cbab96b342270378ccd1
zsofi <zsofiath@gmail.com>
2021-12-12 11:48:55 +0100

    :tada: add border

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,16 @@
 export function addBorder(picture: string[]): string[] {
+  let arr = [];
+  let border ='*'.repeat(picture[0].length+2);
 
+  for (let i = 0; i < picture.length; i++) {
+      const element = picture[i];
+      arr.push(`*${element}*`)
+  }
+
+  arr.unshift(border);
+  arr.push(border);
+
+  return arr;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));

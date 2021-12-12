d6cfcc716508b6bcdebcb3a27a5f497d77b89652
zsofi <zsofiath@gmail.com>
2021-12-12 11:49:34 +0100

    :poop: :poop: :poop: add border - fix test

--- addBorder/addBorder.spec.ts
+++ addBorder/addBorder.spec.ts
@@ -1,14 +1,13 @@
 import { addBorder } from './addBorder';
 
-xdescribe(addBorder.name, () => {
+describe(addBorder.name, () => {
     it('Test 1', () => {
         // arrange
-        const data = ['abc, ded'];
+        const data = ['abc', 'ded'];
 
         // act
         const response = addBorder(data);
 
-        // assert
         expect(response).toEqual([
             "*****",
             "*abc*",

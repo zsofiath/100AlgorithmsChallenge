2befd875d6e2f3703b6cc0d6eab36f89769ee09c
zsofi <zsofiath@gmail.com>
2021-12-14 16:20:42 +0100

    :tada: alphabetic shift

--- alphabeticShift/alphabeticShift.ts
+++ alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,9 @@
 export function alphabeticShift(inputString: string): string {
-
+    let start = 'a'.charCodeAt(0);
+    let end = 'z'.charCodeAt(0)+1;
+    return inputString.split('').map((char, i) => {
+        let code = inputString.charCodeAt(i)+1;
+        return String.fromCharCode(code > 122 ? code - (end-start) : code);
+    }).join('');
 }
-
 console.log(alphabeticShift('crazy'));

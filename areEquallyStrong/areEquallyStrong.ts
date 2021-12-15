c9a8f5d0dd2383712a815ddf7fe3fdf1b552ed92
zsofi <zsofiath@gmail.com>
2021-12-15 19:36:56 +0100

    :tada: are equally strong

--- areEquallyStrong/areEquallyStrong.ts
+++ areEquallyStrong/areEquallyStrong.ts
@@ -1,7 +1,15 @@
-export function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
-
+export function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  return (
+    Math.min(yourLeft, yourRight) == Math.min(friendsLeft, friendsRight) &&
+    Math.max(yourLeft, yourRight) == Math.max(friendsLeft, friendsRight)
+  );
 }
 
-// console.log(areEquallyStrong(10, 15, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 10))
-// console.log(areEquallyStrong(15, 10, 15, 9))
+console.log(areEquallyStrong(10, 15, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 10))
+console.log(areEquallyStrong(15, 10, 15, 9))

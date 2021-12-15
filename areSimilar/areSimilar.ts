5de3e2388ffdd302813a62b7c0a42b89524329c0
zsofi <zsofiath@gmail.com>
2021-12-15 20:11:01 +0100

    :tada: are similar

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,18 @@
 export function areSimilar(a: number[], b: number[]): boolean {
 
+    if(a.toString() == b.toString()) return true;
+    else {
+        for (let i = 1; i < a.length; i++) {
+            let temparr = [...a];
+            let t = temparr[i];
+            temparr[i] = temparr[i-1];
+            temparr[i-1] = t;
+            if(temparr.toString() == b.toString()) return true;
+        }
+    }
+    return false;
 }
 
-// console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-// console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-// console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3], [1, 2, 3]));
+console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2], [2, 1, 1]));

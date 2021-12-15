d6de7fbb4ec85e13ed588dbfd356cdd1784f9867
zsofi <zsofiath@gmail.com>
2021-12-15 18:38:28 +0100

    :recycle: alternating sums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,16 +1,10 @@
 export function alternatingSums(a: number[]): number[] {
-let even = 0;
-let odd = 0;
-
-a.forEach((num, i) => {
-    if(i % 2 == 1) odd+=num;
-    else even+=num;
-});
-
-return [
-    even,
-    odd
-]
+    
+return a.reduce((a, c, i) => {
+    if(i % 2 == 1) a[1]+=c;
+    else a[0]+=c;
+    return a;
+},[0,0]);
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))

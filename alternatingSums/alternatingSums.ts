c7ec063b343209523488d948d12408e7e253f888
zsofi <zsofiath@gmail.com>
2021-12-15 18:33:10 +0100

    :tada: alternating sums

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 export function alternatingSums(a: number[]): number[] {
+let even = [];
+let odd = [];
 
+a.forEach((num, i) => {
+    if(i % 2 == 1) odd.push(num);
+    else even.push(num);
+});
+
+return [
+    even.reduce((a,c)=>a+c, 0),
+    odd.reduce((a,c)=>a+c, 0)
+]
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
